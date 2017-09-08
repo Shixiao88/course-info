521b5ac7d87325f52f37b8f7d61805a398274162
Shixiao88 <Shi Xiao>
2017-09-08 17:18:03 +0800

    2.1

--- src/simpledb/IntegerAggregator.java
+++ src/simpledb/IntegerAggregator.java
@@ -1,5 +1,9 @@
 package simpledb;
 
+import org.omg.CORBA.INTF_REPOS;
+
+import java.util.ArrayList;
+
 /**
  * Knows how to compute some aggregate over a set of IntFields.
  */
@@ -8,8 +12,8 @@ public class IntegerAggregator implements Aggregator {
     private final int afield;
     private final Type gbfieldtype;
     private final Op op;
-    private Tuple tp;
-
+    private ArrayList<Tuple> tuplelist;
+    private boolean isinitiated;
     /**
      * Aggregate constructor
      * @param gbfield the 0-based index of the group-by field in the tuple, or NO_GROUPING if there is no grouping
@@ -23,7 +27,6 @@ public class IntegerAggregator implements Aggregator {
         this.afield = afield;
         this.gbfieldtype = gbfieldtype;
         this.op = what;
-        this.tp = new Tuple (new TupleDesc())
     }
 
     /**
@@ -31,7 +34,36 @@ public class IntegerAggregator implements Aggregator {
      * @param tup the Tuple containing an aggregate field and a group-by field
      */
     public void mergeTupleIntoGroup(Tuple tup) {
+        if (tuplelist.size() == 0) {
+            tuplelist.add(tup);
+        } else {
+            for (Tuple t : tuplelist) {
+                if (tup.getField(gbfindex) == t.getField(gbfindex)) {
+                    
+                }
+            }
+        }
+    }
 
+    private int operation(IntField fd1, IntField fd2, int counter) {
+        switch(op) {
+            case MAX:
+                if (fd1.compare(Predicate.Op.GREATER_THAN_OR_EQ, fd2)) {
+                    return fd1.getValue();
+                }
+                return fd2.getValue();
+            case MIN:
+                if (fd1.compare(Predicate.Op.LESS_THAN_OR_EQ, fd2)) {
+                    return fd1.getValue();
+                } return fd2.getValue();
+            case AVG:
+                return ((fd1.getValue() + fd2.getValue()) / 2);
+            case SUM:
+                return (fd1.getValue() + fd2.getValue());
+            case COUNT:
+                return counter++;
+        }
+        return (Integer)null;
     }
 
     /**
